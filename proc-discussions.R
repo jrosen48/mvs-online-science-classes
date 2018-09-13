577690e5f7e6d06ed1c0ac7231eaade1f44843d1
Joshua Rosenberg <jrosen@msu.edu>
2018-09-13 12:49:38 -0400

    add processed discussions

--- proc-discussions.R
+++ proc-discussions.R
@@ -6,17 +6,12 @@ setwd("~/Dropbox/1_research/utility_value_intervention_online_science/discussion
 
 library(readxl)
 
-file_names <- list.files()
-
-file
-
-out_df <- plyr::ldply(out_files)
-
-str(out_df)
+out_df <- str_c("discussions/", list.files("discussions/")) %>% 
+    map_df(read_excel)
 
 names(out_df) <- c("course", "section", "user_pk", "thread_title", "thread_desc", "post_subject", "date_posted", "text")
 
-write.csv(out_df, "~/Dropbox/research/utility_value_intervention_online_science/all_discussions.csv", row.names = F)
+# write.csv(out_df, "~/Dropbox/research/utility_value_intervention_online_science/all_discussions.csv", row.names = F)
 
 # Loading processed data
 
